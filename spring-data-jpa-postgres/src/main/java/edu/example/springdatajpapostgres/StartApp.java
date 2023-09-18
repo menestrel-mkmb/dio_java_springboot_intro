d72f343560a05329d58b5fa2b185db6093f5c87e
Michael Kevin <michaelkmb@hotmail.com>
2023-09-18 17:53:11 -0300

    JPA Repository example; Users Query example;

--- spring-data-jpa-postgres/src/main/java/edu/example/springdatajpapostgres/StartApp.java
+++ spring-data-jpa-postgres/src/main/java/edu/example/springdatajpapostgres/StartApp.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartApp implements CommandLineRunner {
     @Autowired
@@ -13,15 +15,20 @@ public class StartApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User();
+        List<User> users = userRepo.findByNameContaining("Michael");
 
-        user.setName("Michael Kevin");
-        user.setUsername("michael");
-        user.setPassword("plaintext");
-        userRepo.save(user);
-
-        for(User u: userRepo.findAll()){
+        for(User u: users){
             System.out.println(u);
         }
+    }
+
+    private void insertUser(String name, String username, String password){
+        User user = new User();
+
+        user.setName(name);
+        user.setUsername(username);
+        user.setPassword(password);
+
+        userRepo.save(user);
     }
 }
--- spring-data-jpa-postgres/src/main/java/edu/example/springdatajpapostgres/repository/UserRepository.java
+++ spring-data-jpa-postgres/src/main/java/edu/example/springdatajpapostgres/repository/UserRepository.java
@@ -2,7 +2,17 @@ package edu.example.springdatajpapostgres.repository;
 
 import edu.example.springdatajpapostgres.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByNameContaining(String name);
+
+    User findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE %:name%")
+    List<User> filtrarPorNome(@Param("name") String name);
 
 }
